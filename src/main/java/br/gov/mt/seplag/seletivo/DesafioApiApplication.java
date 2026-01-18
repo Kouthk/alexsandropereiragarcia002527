3443b7309dba5b8b7109da7a02f00cd0423f0857
Kouthk <aalex.spg@gmail.com>
2026-01-18 09:31:13 -0400

    feat(projeto): configuração inicial do Spring Boot com health check

---
+++ src/main/java/br/gov/mt/seplag/seletivo/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.seletivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApiApplication.class, args);
+    }
+}
