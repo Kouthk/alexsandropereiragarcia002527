e1608b3a1529e18ab3d09c7e8ff2922d49b486dd
Kouthk <alex_spg@outlook.com>
2026-02-04 19:01:15 -0400

    chore(app): ajustar inicialização da aplicação

--- src/main/java/br/gov/mt/seplag/seletivo/DesafioApiApplication.java
+++ src/main/java/br/gov/mt/seplag/seletivo/DesafioApiApplication.java
@@ -2,9 +2,11 @@ package br.gov.mt.seplag.seletivo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DesafioApiApplication {
 
     public static void main(String[] args) {
