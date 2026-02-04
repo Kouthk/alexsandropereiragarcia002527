6063d78690dc4879810d3c2e5d7b2fcc409b96cc
Kouthk <aalex.spg@gmail.com>
2026-02-04 02:24:55 -0400

    Refactor(DTOs): adicionar descrições e metadados para o Swagger

--- src/main/java/br/gov/mt/seplag/seletivo/dto/RegionalResponseDTO.java
+++ src/main/java/br/gov/mt/seplag/seletivo/dto/RegionalResponseDTO.java
@@ -1,9 +1,15 @@
 package br.gov.mt.seplag.seletivo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RegionalResponseDTO(
+        @Schema(description = "ID interno", example = "10")
         Long id,
+        @Schema(description = "ID no serviço externo", example = "123")
         Integer idRegionalExterno,
+        @Schema(description = "Nome da regional", example = "Cuiabá")
         String nome,
+        @Schema(description = "Indica se está ativa", example = "true")
         Boolean ativo
 ) {
 }
